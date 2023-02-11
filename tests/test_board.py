259ac509309333890e576a7d71bc3ee0d7acf8fc
Simen Refsland <simen_refsland@hotmail.com>
2023-02-11 19:18:20 +0100

    change tests to pytest

--- tests/test_board.py
+++ tests/test_board.py
@@ -1,147 +1,150 @@
 from src.statemanager.hexstatemanager import HexStateManager
-import unittest
+import pytest
 
 
-class BoardTest(unittest.TestCase):
-    def test_win_state_player1(self):
-        setup_board_win_state_check = HexStateManager(4)
+def test_win_state_player1():
+    setup_board_win_state_check = HexStateManager(4)
 
-        setup_board_win_state_check.board[0][0].set_owner((1, 0))
-        setup_board_win_state_check.board[1][0].set_owner((1, 0))
-        setup_board_win_state_check.board[2][0].set_owner((1, 0))
-        setup_board_win_state_check.board[1][1].set_owner((1, 0))
-        setup_board_win_state_check.board[2][1].set_owner((1, 0))
-        setup_board_win_state_check.board[1][2].set_owner((1, 0))
-        setup_board_win_state_check.board[1][3].set_owner((1, 0))
+    setup_board_win_state_check.board[0][0].set_owner((1, 0))
+    setup_board_win_state_check.board[1][0].set_owner((1, 0))
+    setup_board_win_state_check.board[2][0].set_owner((1, 0))
+    setup_board_win_state_check.board[1][1].set_owner((1, 0))
+    setup_board_win_state_check.board[2][1].set_owner((1, 0))
+    setup_board_win_state_check.board[1][2].set_owner((1, 0))
+    setup_board_win_state_check.board[1][3].set_owner((1, 0))
 
-        is_win = setup_board_win_state_check.check_winning_state_player1()
+    is_win = setup_board_win_state_check.check_winning_state_player1()
 
-        self.assertFalse(is_win)
+    assert is_win is False
 
-        setup_board_win_state_check = HexStateManager(4)
+    setup_board_win_state_check = HexStateManager(4)
 
-        setup_board_win_state_check.board[0][0].set_owner((1, 0))
-        setup_board_win_state_check.board[1][0].set_owner((1, 0))
-        setup_board_win_state_check.board[2][0].set_owner((1, 0))
-        setup_board_win_state_check.board[1][1].set_owner((1, 0))
-        setup_board_win_state_check.board[2][1].set_owner((1, 0))
-        setup_board_win_state_check.board[1][2].set_owner((1, 0))
-        setup_board_win_state_check.board[2][2].set_owner((1, 0))
+    setup_board_win_state_check.board[0][0].set_owner((1, 0))
+    setup_board_win_state_check.board[1][0].set_owner((1, 0))
+    setup_board_win_state_check.board[2][0].set_owner((1, 0))
+    setup_board_win_state_check.board[1][1].set_owner((1, 0))
+    setup_board_win_state_check.board[2][1].set_owner((1, 0))
+    setup_board_win_state_check.board[1][2].set_owner((1, 0))
+    setup_board_win_state_check.board[2][2].set_owner((1, 0))
 
-        is_win = setup_board_win_state_check.check_winning_state_player1()
+    is_win = setup_board_win_state_check.check_winning_state_player1()
 
-        self.assertFalse(is_win)
+    assert is_win is False
 
-        setup_board_win_state_check = HexStateManager(4)
+    setup_board_win_state_check = HexStateManager(4)
 
-        setup_board_win_state_check.board[0][0].set_owner((1, 0))
-        setup_board_win_state_check.board[1][0].set_owner((1, 0))
-        setup_board_win_state_check.board[2][0].set_owner((1, 0))
-        setup_board_win_state_check.board[1][1].set_owner((1, 0))
-        setup_board_win_state_check.board[2][1].set_owner((1, 0))
-        setup_board_win_state_check.board[1][2].set_owner((1, 0))
-        setup_board_win_state_check.board[2][2].set_owner((1, 0))
-        setup_board_win_state_check.board[3][1].set_owner((1, 0))
+    setup_board_win_state_check.board[0][0].set_owner((1, 0))
+    setup_board_win_state_check.board[1][0].set_owner((1, 0))
+    setup_board_win_state_check.board[2][0].set_owner((1, 0))
+    setup_board_win_state_check.board[1][1].set_owner((1, 0))
+    setup_board_win_state_check.board[2][1].set_owner((1, 0))
+    setup_board_win_state_check.board[1][2].set_owner((1, 0))
+    setup_board_win_state_check.board[2][2].set_owner((1, 0))
+    setup_board_win_state_check.board[3][1].set_owner((1, 0))
 
-        is_win = setup_board_win_state_check.check_winning_state_player1()
+    is_win = setup_board_win_state_check.check_winning_state_player1()
 
-        self.assertTrue(is_win)
+    assert is_win is True
 
-    def test_win_state_player2(self):
-        setup_board_win_state_check = HexStateManager(4)
 
-        setup_board_win_state_check.board[0][0].set_owner((0, 1))
-        setup_board_win_state_check.board[1][0].set_owner((0, 1))
-        setup_board_win_state_check.board[2][0].set_owner((0, 1))
-        setup_board_win_state_check.board[1][1].set_owner((0, 1))
-        setup_board_win_state_check.board[2][1].set_owner((0, 1))
-        setup_board_win_state_check.board[1][2].set_owner((0, 1))
-        setup_board_win_state_check.board[1][3].set_owner((0, 1))
-        setup_board_win_state_check.board[0][3].set_owner((0, 1))
+def test_win_state_player2():
+    setup_board_win_state_check = HexStateManager(4)
 
-        is_win = setup_board_win_state_check.check_winning_state_player2()
+    setup_board_win_state_check.board[0][0].set_owner((0, 1))
+    setup_board_win_state_check.board[1][0].set_owner((0, 1))
+    setup_board_win_state_check.board[2][0].set_owner((0, 1))
+    setup_board_win_state_check.board[1][1].set_owner((0, 1))
+    setup_board_win_state_check.board[2][1].set_owner((0, 1))
+    setup_board_win_state_check.board[1][2].set_owner((0, 1))
+    setup_board_win_state_check.board[1][3].set_owner((0, 1))
+    setup_board_win_state_check.board[0][3].set_owner((0, 1))
 
-        self.assertTrue(is_win)
+    is_win = setup_board_win_state_check.check_winning_state_player2()
 
-        setup_board_win_state_check = HexStateManager(4)
+    assert is_win is True
 
-        setup_board_win_state_check.board[0][0].set_owner((0, 1))
-        setup_board_win_state_check.board[1][0].set_owner((0, 1))
-        setup_board_win_state_check.board[2][0].set_owner((0, 1))
-        setup_board_win_state_check.board[1][1].set_owner((0, 1))
-        setup_board_win_state_check.board[2][1].set_owner((0, 1))
-        setup_board_win_state_check.board[1][2].set_owner((0, 1))
-        setup_board_win_state_check.board[2][2].set_owner((0, 1))
-        setup_board_win_state_check.board[3][2].set_owner((0, 1))
+    setup_board_win_state_check = HexStateManager(4)
 
-        is_win = setup_board_win_state_check.check_winning_state_player2()
+    setup_board_win_state_check.board[0][0].set_owner((0, 1))
+    setup_board_win_state_check.board[1][0].set_owner((0, 1))
+    setup_board_win_state_check.board[2][0].set_owner((0, 1))
+    setup_board_win_state_check.board[1][1].set_owner((0, 1))
+    setup_board_win_state_check.board[2][1].set_owner((0, 1))
+    setup_board_win_state_check.board[1][2].set_owner((0, 1))
+    setup_board_win_state_check.board[2][2].set_owner((0, 1))
+    setup_board_win_state_check.board[3][2].set_owner((0, 1))
 
-        self.assertFalse(is_win)
+    is_win = setup_board_win_state_check.check_winning_state_player2()
 
-    def test_conversion_to_diamond_shape(self):
-        setup_board_win_state_check = HexStateManager(4)
+    assert is_win is False
 
-        diamond_array = setup_board_win_state_check.convert_to_diamond_shape()
 
-        self.assertEqual(diamond_array[0][0].get_position(), (0, 0))
+def test_conversion_to_diamond_shape():
+    setup_board_win_state_check = HexStateManager(4)
 
-        self.assertEqual(diamond_array[1][0].get_position(), (1, 0))
-        self.assertEqual(diamond_array[1][1].get_position(), (0, 1))
+    diamond_array = setup_board_win_state_check.convert_to_diamond_shape()
 
-        self.assertEqual(diamond_array[2][0].get_position(), (2, 0))
-        self.assertEqual(diamond_array[2][1].get_position(), (1, 1))
-        self.assertEqual(diamond_array[2][2].get_position(), (0, 2))
+    assert diamond_array[0][0].get_position() == (0, 0)
 
-        self.assertEqual(diamond_array[3][0].get_position(), (3, 0))
-        self.assertEqual(diamond_array[3][1].get_position(), (2, 1))
-        self.assertEqual(diamond_array[3][2].get_position(), (1, 2))
-        self.assertEqual(diamond_array[3][3].get_position(), (0, 3))
+    assert diamond_array[1][0].get_position() == (1, 0)
+    assert diamond_array[1][1].get_position() == (0, 1)
 
-        self.assertEqual(diamond_array[4][0].get_position(), (3, 1))
-        self.assertEqual(diamond_array[4][1].get_position(), (2, 2))
-        self.assertEqual(diamond_array[4][2].get_position(), (1, 3))
+    assert diamond_array[2][0].get_position() == (2, 0)
+    assert diamond_array[2][1].get_position() == (1, 1)
+    assert diamond_array[2][2].get_position() == (0, 2)
 
-        self.assertEqual(diamond_array[5][0].get_position(), (3, 2))
-        self.assertEqual(diamond_array[5][1].get_position(), (2, 3))
+    assert diamond_array[3][0].get_position() == (3, 0)
+    assert diamond_array[3][1].get_position() == (2, 1)
+    assert diamond_array[3][2].get_position() == (1, 2)
+    assert diamond_array[3][3].get_position() == (0, 3)
 
-        self.assertEqual(diamond_array[6][0].get_position(), (3, 3))
+    assert diamond_array[4][0].get_position() == (3, 1)
+    assert diamond_array[4][1].get_position() == (2, 2)
+    assert diamond_array[4][2].get_position() == (1, 3)
 
-    def test_conversion_to_1D_array(self):
-        board = HexStateManager(4)
+    assert diamond_array[5][0].get_position() == (3, 2)
+    assert diamond_array[5][1].get_position() == (2, 3)
 
-        new_board = board.convert_to_1D_array()
+    assert diamond_array[6][0].get_position() == (3, 3)
 
-        self.assertEqual(new_board.shape, (16,))
 
-        self.assertEqual(new_board[0].get_position(), (0, 0))
-        self.assertEqual(new_board[1].get_position(), (0, 1))
-        self.assertEqual(new_board[2].get_position(), (0, 2))
-        self.assertEqual(new_board[3].get_position(), (0, 3))
-        self.assertEqual(new_board[4].get_position(), (1, 0))
-        self.assertEqual(new_board[5].get_position(), (1, 1))
-        self.assertEqual(new_board[6].get_position(), (1, 2))
-        self.assertEqual(new_board[7].get_position(), (1, 3))
-        self.assertEqual(new_board[8].get_position(), (2, 0))
-        self.assertEqual(new_board[9].get_position(), (2, 1))
-        self.assertEqual(new_board[10].get_position(), (2, 2))
-        self.assertEqual(new_board[11].get_position(), (2, 3))
-        self.assertEqual(new_board[12].get_position(), (3, 0))
-        self.assertEqual(new_board[13].get_position(), (3, 1))
-        self.assertEqual(new_board[14].get_position(), (3, 2))
-        self.assertEqual(new_board[15].get_position(), (3, 3))
+def test_conversion_to_1D_array():
+    board = HexStateManager(4)
 
-    def test_move_making(self):
-        board = HexStateManager(4)
+    new_board = board.convert_to_1D_array()
 
+    assert new_board.shape == (16,)
+
+    assert new_board[0].get_position() == (0, 0)
+    assert new_board[1].get_position() == (0, 1)
+    assert new_board[2].get_position() == (0, 2)
+    assert new_board[3].get_position() == (0, 3)
+    assert new_board[4].get_position() == (1, 0)
+    assert new_board[5].get_position() == (1, 1)
+    assert new_board[6].get_position() == (1, 2)
+    assert new_board[7].get_position() == (1, 3)
+    assert new_board[8].get_position() == (2, 0)
+    assert new_board[9].get_position() == (2, 1)
+    assert new_board[10].get_position() == (2, 2)
+    assert new_board[11].get_position() == (2, 3)
+    assert new_board[12].get_position() == (3, 0)
+    assert new_board[13].get_position() == (3, 1)
+    assert new_board[14].get_position() == (3, 2)
+    assert new_board[15].get_position() == (3, 3)
+
+
+def test_move_making():
+    board = HexStateManager(4)
+
+    board.make_move((0, 0), (0, 1))
+    assert board.board[0][0].get_owner() == (0, 1)
+
+    with pytest.raises(Exception):
         board.make_move((0, 0), (0, 1))
-        self.assertEqual(board.board[0][0].get_owner(), (0, 1))
 
-        with self.assertRaises(Exception):
-            board.make_move((0, 0), (0, 1))
-
-        board.make_move((0, 1), (1, 0))
-        self.assertEqual(board.board[0][1].get_owner(), (1, 0))
+    board.make_move((0, 1), (1, 0))
+    assert board.board[0][1].get_owner() == (1, 0)
 
 
-if __name__ == '__main__':
-    unittest.main()
+# if __name__ == '__main__':
+#     unittest.main()
