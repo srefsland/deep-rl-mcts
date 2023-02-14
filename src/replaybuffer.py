1586778fbd2284b6d899b4a2fefb12d3cb71449c
Simen Refsland <simen_refsland@hotmail.com>
2023-02-14 17:51:55 +0100

    update replay buffer to use deque, better time complexity

--- src/replaybuffer.py
+++ src/replaybuffer.py
@@ -1,20 +1,19 @@
 import random
+from collections import deque
 
 
 class ReplayBufffer:
     def __init__(self, maxlen=400):
         self.maxlen = maxlen
-        self.replay_buffer = []
+        # Deque should be more efficient than the previous list, since the time complexity of appending and popping from a deque is constant
+        self.replay_buffer = deque(maxlen=maxlen)
 
     def clear(self):
-        self.replay_buffer = []
+        self.replay_buffer = deque(maxlen=self.maxlen)
 
     # A case should be a game state (root state of current game) combined with the target distribution D, derived from MCTS simulations
     def add_case(self, case):
         self.replay_buffer.append(case)
 
-        if len(self.replay_buffer) > self.maxlen:
-            self.replay_buffer.pop(0)
-
     def get_random_minibatch(self, batch_size):
-        return random.sample(self.replay_buffer, batch_size) if len(self.replay_buffer) > batch_size else self.replay_buffer
+        return random.sample(self.replay_buffer, batch_size) if len(self.replay_buffer) > batch_size else list(self.replay_buffer)
