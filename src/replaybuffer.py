01ef101823a0157fa4ca82038a8ea9d66172a4d6
Simen Refsland <simen_refsland@hotmail.com>
2023-02-14 17:56:43 +0100

    remove maxlen attribute from replay buffer

--- src/replaybuffer.py
+++ src/replaybuffer.py
@@ -4,12 +4,11 @@ from collections import deque
 
 class ReplayBufffer:
     def __init__(self, maxlen=400):
-        self.maxlen = maxlen
         # Deque should be more efficient than the previous list, since the time complexity of appending and popping from a deque is constant
         self.replay_buffer = deque(maxlen=maxlen)
 
     def clear(self):
-        self.replay_buffer = deque(maxlen=self.maxlen)
+        self.replay_buffer.clear()
 
     # A case should be a game state (root state of current game) combined with the target distribution D, derived from MCTS simulations
     def add_case(self, case):
