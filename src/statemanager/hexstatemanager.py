7da7852aa37f77df20d96d85eb471f354779a9f4
Simen Refsland <simen_refsland@hotmail.com>
2025-11-10 21:53:56 +0100

    speedup of hexstatemanager

--- src/statemanager/hexstatemanager.py
+++ src/statemanager/hexstatemanager.py
@@ -1,27 +1,31 @@
 import pickle
 
 import numpy as np
-from disjoint_set import DisjointSet
 
 from .exceptions import IllegalMoveException
-
 from .statemanager import StateManager
 
 
-class MoveEntry:
-    def __init__(self, move, player_moved, switch_move_type):
-        self.move = move
-        self.player_moved = player_moved
-        self.switch_move_type = switch_move_type
-
-
 class HexStateManager(StateManager):
-    def __init__(self, board_size=6):
+    def __init__(self, board_size=6, initialize=True):
         self.board_size = board_size
-        self._initialize_state(board_size)
+        if initialize:
+            self._initialize_state(board_size)
+
+    def copy_state_manager2(self):
+        return pickle.loads(pickle.dumps(self, protocol=pickle.HIGHEST_PROTOCOL))
 
     def copy_state_manager(self):
-        return pickle.loads(pickle.dumps(self))
+        copied_state_manager = HexStateManager(self.board_size, initialize=False)
+        copied_state_manager.board = self.board.copy()
+
+        copied_state_manager.switched = self.switched
+        copied_state_manager.legal_moves = set(self.legal_moves)
+        copied_state_manager.first_move = self.first_move
+        copied_state_manager.move_count = self.move_count
+        copied_state_manager.player_to_move = self.player_to_move
+
+        return copied_state_manager
 
     # NOTE: only passes player as parameter to be able to generalize for all types of state manager in 2v2 board games.
     # In Hex, the available moves are the same for both players.
@@ -35,18 +39,21 @@ class HexStateManager(StateManager):
         if move not in self.legal_moves:
             raise IllegalMoveException()
 
-        is_first_move = self.first_move is None
-        is_second_move = len(self.move_history) == 1
+        is_first_move = self.move_count == 0
+        is_second_move = self.move_count == 1
 
-        if is_first_move:
+        if not is_first_move and not is_second_move:
+            self.board[move] = player_to_move
+            self.legal_moves.remove(move)
+        elif is_first_move:
             self.first_move = move
             self.board[move] = player_to_move
-        elif is_second_move:
+        else:
             if move == self.first_move:
                 self.switched = True
 
                 mirrored_move = (move[1], move[0])
-                
+
                 if mirrored_move != move:
                     self.board[move] = 0
                     self.board[mirrored_move] = player_to_move
@@ -54,29 +61,26 @@ class HexStateManager(StateManager):
                     self.board[move] = player_to_move
 
                 self.legal_moves.remove(mirrored_move)
-                self._union_move(mirrored_move, player_to_move)
-                
+
                 move = mirrored_move
             else:
                 self.board[move] = player_to_move
                 self.legal_moves.remove(move)
                 self.legal_moves.remove(self.first_move)
 
-                self._union_move(move, player_to_move)
-                self._union_move(self.first_move, 1)
-        else:
-            self.board[move] = player_to_move
-            self.legal_moves.remove(move)
-            self._union_move(move, player_to_move)
-
-        self.move_history.append(
-            MoveEntry(
-                move, player_to_move, self.switched and len(self.move_history) == 1
-            )
-        )
+        self.move_count += 1
         self._change_player_to_move(player_to_move)
 
         return move
+
+    def undo_move(self, move, player_to_move=None):
+        if player_to_move is None:
+            player_to_move = -self.player_to_move  # The player who made the move
+
+        self.board[move] = 0
+        self.legal_moves.add(move)
+        self.move_count -= 1
+        self._change_player_to_move(player_to_move)
 
     def make_random_move(self, player_to_move=None):
         if player_to_move is None:
@@ -92,16 +96,44 @@ class HexStateManager(StateManager):
         )
 
         return move
-
+    
     def generate_child_states(self, player_to_move=None):
         if player_to_move is None:
             player_to_move = self.player_to_move
 
         for move in self.get_legal_moves():
-            state_manager = self.copy_state_manager()
-            state_manager.make_move(move, player_to_move)
+            child_state_manager = self.copy_state_manager()
+            
+            child_state_manager.make_move(move, player_to_move)
+            yield child_state_manager.board, child_state_manager.player_to_move, move
 
-            yield state_manager.board, state_manager.player_to_move, move
+    def generate_child_states2(self, player_to_move=None):
+        if player_to_move is None:
+            player_to_move = self.player_to_move
+
+        move_count = self.move_count
+        first_move = self.first_move
+
+        for move in self.get_legal_moves():
+            child_board = self.board.copy()
+
+            if move_count == 1:
+                if move == first_move:
+                    mirrored_move = (move[1], move[0])
+
+                    if mirrored_move != move:
+                        child_board[move] = 0
+                        child_board[mirrored_move] = player_to_move
+                    else:
+                        child_board[move] = player_to_move
+                else:
+                    child_board[move] = player_to_move
+            else:
+                child_board[move] = player_to_move
+
+            player_to_move_next = -player_to_move
+
+            yield child_board, player_to_move_next, move
 
     def check_winning_state(self, player_moved=None):
         if player_moved == 1:
@@ -130,42 +162,14 @@ class HexStateManager(StateManager):
             [(i, j) for j in range(board_size) for i in range(board_size)]
         )
         self.first_move = None
-        self.move_history = []
+        self.move_count = 0
         self.player_to_move = 1
 
-        self.top_node = (-1, 0)
-        self.bottom_node = (board_size, 0)
-        self.left_node = (0, -1)
-        self.right_node = (0, board_size)
-
-        cells = [(i, j) for j in range(board_size) for i in range(board_size)]
-        self.disjoint_set_red = DisjointSet(cells + [self.top_node, self.bottom_node])
-        self.disjoint_set_blue = DisjointSet(cells + [self.left_node, self.right_node])
-
-        for i in range(board_size):
-            self.disjoint_set_red.union((0, i), self.top_node)
-            self.disjoint_set_red.union((board_size - 1, i), self.bottom_node)
-            self.disjoint_set_blue.union((i, 0), self.left_node)
-            self.disjoint_set_blue.union((i, board_size - 1), self.right_node)
-
     def _check_winning_state_player1(self):
-        return self.disjoint_set_red.find(self.top_node) == self.disjoint_set_red.find(
-            self.bottom_node
-        )
+        return self._dfs(1)
 
     def _check_winning_state_player2(self):
-        return self.disjoint_set_blue.find(
-            self.left_node
-        ) == self.disjoint_set_blue.find(self.right_node)
-
-    def _union_move(self, move, player):
-        neighbors = self._expand_neighbors(move, player)
-        if player == 1:
-            for neighbor in neighbors:
-                self.disjoint_set_red.union(move, neighbor)
-        else:
-            for neighbor in neighbors:
-                self.disjoint_set_blue.union(move, neighbor)
+        return self._dfs(-1)
 
     def _expand_neighbors(self, cell, player_to_move=None):
         if player_to_move is None:
@@ -192,6 +196,44 @@ class HexStateManager(StateManager):
                 neighbors.append(neighbor)
 
         return neighbors
+
+    def _dfs(self, player):
+        visited = set()
+        stack = []
+
+        if player == 1:
+            for i in range(self.board_size):
+                if self.board[0, i] == 1:
+                    stack.append((0, i))
+                    visited.add((0, i))
+
+            while stack:
+                cell = stack.pop()
+                if cell[0] == self.board_size - 1:
+                    return True
+
+                for neighbor in self._expand_neighbors(cell, player):
+                    if neighbor not in visited:
+                        visited.add(neighbor)
+                        stack.append(neighbor)
+
+        else:
+            for i in range(self.board_size):
+                if self.board[i, 0] == -1:
+                    stack.append((i, 0))
+                    visited.add((i, 0))
+
+            while stack:
+                cell = stack.pop()
+                if cell[1] == self.board_size - 1:
+                    return True
+
+                for neighbor in self._expand_neighbors(cell, player):
+                    if neighbor not in visited:
+                        visited.add(neighbor)
+                        stack.append(neighbor)
+
+        return False
 
     def _is_within_bounds(self, cell):
         row, col = cell
--- src/train.py
+++ src/train.py
@@ -4,11 +4,8 @@ from datetime import datetime
 
 import numpy as np
 import torch
-from tqdm import tqdm
 from torch.utils.data import DataLoader, TensorDataset
-
-from nn.train import train
-from nn.nn_options_torch import optimizers, loss_functions, activation_functions
+from tqdm import tqdm
 
 import config
 import replay_buffer
@@ -17,19 +14,30 @@ from display.gameboarddisplay import GameBoardDisplay
 from display.hexboarddisplay import HexBoardDisplay
 from display.hexboarddisplayclassic import HexBoardDisplayClassic
 from mcts.mcts import MCTS
+from nn.board_encoder import convert_board_state_to_tensor
 from nn.hexresnet import HexResNet
+from nn.nn_options_torch import loss_functions, optimizers
+from nn.train import train
 from statemanager.hexstatemanager import HexStateManager
 from statemanager.statemanager import StateManager
-from nn.board_encoder import convert_board_state_to_tensor
 
-device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
+device = torch.device(
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps" if torch.backends.mps.is_available() else "cpu"
+)
 
 
-def rl_algorithm(actor: Actor, state_manager: StateManager, mcts_state_manager: StateManager, display: GameBoardDisplay):
+def rl_algorithm(
+    actor: Actor,
+    state_manager: StateManager,
+    mcts_state_manager: StateManager,
+    display: GameBoardDisplay,
+):
     logging.basicConfig(
         level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
     )
-    
+
     logging.info(f"Using device: {actor.nn.device}")
 
     replay_buf = replay_buffer.ReplayBuffer(maxlen=config.REPLAY_BUFFER_SIZE)
@@ -62,8 +70,9 @@ def rl_algorithm(actor: Actor, state_manager: StateManager, mcts_state_manager: 
                 time.time() - start_time < config.MCTS_DYNAMIC_SIMS_TIME
                 or i < config.MCTS_MIN_SIMULATIONS
             ):
-                i += 1
+                # call simulation then increment counter so `i` equals performed simulations
                 mcts_tree.simulation_iteration(actor)
+                i += 1
 
             logging.info(
                 f"Number of simulations: {i}, time: {(time.time() - start_time):.2f} seconds"
@@ -72,10 +81,16 @@ def rl_algorithm(actor: Actor, state_manager: StateManager, mcts_state_manager: 
             moves += 1
 
             distribution = mcts_tree.get_visit_distribution(mcts_tree.root)
-            
-            board_state = convert_board_state_to_tensor(
-                mcts_tree.root.state, mcts_tree.root.player_to_move
-            ).detach().cpu().numpy().astype(np.float32)
+
+            board_state = (
+                convert_board_state_to_tensor(
+                    state_manager.board.copy(), state_manager.player_to_move
+                )
+                .detach()
+                .cpu()
+                .numpy()
+                .astype(np.float32)
+            )
 
             replay_buf.add_case(
                 (
@@ -91,6 +106,8 @@ def rl_algorithm(actor: Actor, state_manager: StateManager, mcts_state_manager: 
                 else mcts_tree.select_random_best_distribution()
             )
 
+            # s_move = state_manager.first_move if state_manager.move_count == 1 else s_move
+
             state_manager.make_move(s_move)
             mcts_tree.prune_tree(s_move)
 
@@ -98,48 +115,53 @@ def rl_algorithm(actor: Actor, state_manager: StateManager, mcts_state_manager: 
                 display.display_board(state_manager, delay=0.1, newest_move=s_move)
 
         X, y_actor, y_critic = replay_buf.get_random_minibatch(config.MINI_BATCH_SIZE)
-        
+
         # Create train data loader torch
-        train_dataset = TensorDataset(torch.tensor(X).float(), torch.tensor(y_actor).float(), torch.tensor(y_critic).float())
-        train_loader = DataLoader(train_dataset, batch_size=config.MINI_BATCH_SIZE, shuffle=True)
-        
-        # Train actor and critic
-        train(
-            model=actor.nn,
-            train_loader=train_loader,
-            optimizer=optimizers[config.ANN_OPTIMIZER](actor.nn.parameters(), lr=config.LEARNING_RATE),
-            device=actor.nn.device,
-            epochs=config.NUM_EPOCHS,
-            loss_actor_fn=loss_functions[config.LOSS_FUNCTION_ACTOR](),
-            loss_critic_fn=loss_functions[config.LOSS_FUNCTION_CRITIC](),
-            checkpoint_dir=config.MODEL_DIR if g_a % i_s == 0 else None,
+        train_dataset = TensorDataset(
+            torch.tensor(X).float(),
+            torch.tensor(y_actor).float(),
+            torch.tensor(y_critic).float(),
         )
-        
+        train_loader = DataLoader(
+            train_dataset, batch_size=config.MINI_BATCH_SIZE, shuffle=True
+        )
+
+        # # Train actor and critic
+        # train(
+        #     model=actor.nn,
+        #     train_loader=train_loader,
+        #     optimizer=optimizers[config.ANN_OPTIMIZER](actor.nn.parameters(), lr=config.LEARNING_RATE),
+        #     device=actor.nn.device,
+        #     epochs=config.NUM_EPOCHS,
+        #     loss_actor_fn=loss_functions[config.LOSS_FUNCTION_ACTOR](),
+        #     loss_critic_fn=loss_functions[config.LOSS_FUNCTION_CRITIC](),
+        #     checkpoint_dir=config.MODEL_DIR if g_a % i_s == 0 else None,
+        # )
 
 
 if __name__ == "__main__":
-    cnn_filter_size = config.CNN_NUM_FILTERS    
+    cnn_filter_size = config.CNN_NUM_FILTERS
     num_res_blocks = config.NUM_RES_BLOCKS
-    
+
     nn = HexResNet(
         board_size=config.BOARD_SIZE,
         in_channels=5,
         num_filters=cnn_filter_size,
         num_res_blocks=num_res_blocks,
     )
-    
+
     nn.to(device)
     nn.device = device
-    
+
     state_manager = HexStateManager(config.BOARD_SIZE)
     mcts_state_manager = HexStateManager(config.BOARD_SIZE)
-    
+
     display = (
         None
         if not config.DISPLAY_GAME_RL
         else HexBoardDisplayClassic() if config.CLASSIC_DISPLAY else HexBoardDisplay()
     )
-    
+
     actor = Actor(
         name="actor_rl",
         nn=nn,
@@ -149,7 +171,7 @@ if __name__ == "__main__":
         epsilon_critic=config.EPSILON_CRITIC,
         epsilon_decay_critic=config.EPSILON_DECAY_CRITIC,
     )
-    
+
     rl_algorithm(
         actor=actor,
         state_manager=state_manager,
